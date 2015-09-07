1ac1a62bf8f07a6ec104b8e55efdb9371fc784b4
Ali Bahman <abn@webit4.me>
2015-09-07 12:28:19 +0100

    using older version of dev-tools

--- vagrant/puphpet/puppet/modules/supervisord/manifests/params.pp
+++ vagrant/puphpet/puppet/modules/supervisord/manifests/params.pp
@@ -46,7 +46,8 @@ class supervisord::params {
   $umask                = '022'
   $config_include       = '/etc/supervisor.d'
   $config_file          = '/etc/supervisord.conf'
-  $setuptools_url       = 'https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py'
+# $setuptools_url       = 'https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py'
+  $setuptools_url       = 'https://bitbucket.org/pypa/setuptools/raw/e9fb153e7121a502977dde734996e54cd88aac26/ez_setup.py'
 
   $unix_socket          = true
   $unix_socket_file     = 'supervisor.sock'
