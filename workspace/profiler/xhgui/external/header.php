14de46cf498d252a7cd9f79668e7c2d1ba79d2b5
Ali Bahman <abn@webit4.me>
2015-10-13 14:21:58 +0100

    fixing server name check

--- workspace/profiler/xhgui/external/header.php
+++ workspace/profiler/xhgui/external/header.php
@@ -1,6 +1,6 @@
 <?php
 
-if (in_array($_SERVER['SERVER_NAME'], [
+if (array_key_exists('SERVER_NAME', $_SERVER) && in_array($_SERVER['SERVER_NAME'], [
     'xhgui.local',
     'www.xhgui.local',
     'webgrind.local',
